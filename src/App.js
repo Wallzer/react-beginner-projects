af5aa84ade58608f83fa835e76465bba76786f50
Wallzer <faulkree320@gmail.com>
2025-08-01 17:13:29 +0300

    counter_done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './index.scss';
-
+import React from 'react';
 function App() {
+    const [score,SetScore] = React.useState(0)
+    function PlusScore(){
+        SetScore(score+1);
+    }
+    function MinusScore(){
+        SetScore(score-1);
+    }
+
   return (
+
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{score}</h1>
+        <button onClick={MinusScore} className="minus">- Минус</button>
+        <button onClick={PlusScore} className="plus">Плюс +</button>
       </div>
     </div>
   );
