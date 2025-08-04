fd729aa6a0760c7aa1fdd61a2d4feb5aecbbcf43
Wallzer <faulkree320@gmail.com>
2025-08-04 09:48:04 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.scss';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 const questions = [
   {
     title: 'React - это ... ?',
@@ -22,35 +22,39 @@ const questions = [
   },
 ];
 
-function Result({score}) {
+function Result({ score }) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVisible(true);
+    }, 50);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали {score} ответа из 10</h2>
+    <div className={`result ${visible ? 'show' : ''}`}>
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="result" />
+      <h2>Вы отгадали {score} ответа из {questions.length}</h2>
       <button>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
-    const [open, setOpen] = React.useState(false)
-    const [currentQuestion, setCurrent] = React.useState(0)
-    const maxlenght = questions.length
-    const [score,setScore] = React.useState(0)
-    function getScore() {
-        return score;
-    }
-    let count = 0;
+function Game({setCount,count,currentQuestion,setScore,score,setCurrent,maxlenght}) {
+    const percentage = Math.round((currentQuestion / maxlenght) * 100);
     function questionUpdate(x){
         if (x===questions[currentQuestion].correct){
             setScore(score+1);
             setCurrent(currentQuestion+1);
-            count++;
+            setCount(count+1);
+
+
         }else if (count===maxlenght) {
 
         }else{
             setCurrent(currentQuestion+1)
-            count++
+            setCount(count+1);
 
         }
 
@@ -62,7 +66,7 @@ function Game() {
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
       <h1>{questions[currentQuestion].title}</h1>
       <ul>
@@ -76,13 +80,16 @@ function Game() {
 }
 
 function App() {
-
-
-
+    // const [open, setOpen] = React.useState(false)
+    const [currentQuestion, setCurrent] = React.useState(0)
+    const maxlenght = questions.length
+    const [score,setScore] = React.useState(0)
+    const [count,setCount] = React.useState(0)
   return (
     <div className="App">
-        <Game />
-         <Result score={score} />
+        {count===maxlenght ? (<Result score={score} />):
+        (<Game setCount={setCount} count={count} currentQuestion={currentQuestion} score={score} setScore={setScore} setCurrent={setCurrent} maxlenght={maxlenght} />)}
+
     </div>
   );
 }
