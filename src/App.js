c6f69e5e39d2fe981697deb19fdfe2d03dc3ac83
Wallzer <faulkree320@gmail.com>
2025-08-06 08:59:40 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [users, setUsers] = React.useState([]);
+    const [isLoading, setLoading] = React.useState(true);
+    const [searchValue, setSearchValue] = React.useState(''); // исправлено начальное значение на пустую строку
+
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users?page=1', {
+            headers: {
+                'x-api-key': 'reqres-free-v1'
+            }
+        })
+        .then(res => res.json())
+        .then(json => {
+            setUsers(json.data);
+        })
+        .catch(err => console.warn(err))
+        .finally(() => setLoading(false));
+    }, []); // добавлен пустой массив зависимостей
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value);
+    }
+
+    return (
+        <div className="App">
+            <Users
+                items={users}
+                isLoading={isLoading}
+                searchValue={searchValue}
+                onChangeSearchValue={onChangeSearchValue}
+            />
+            {/* <Success /> */}
+        </div>
+    );
 }
 
 export default App;
